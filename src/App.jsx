ecc71f328179fd05e097412b2b3f0155915a49b9
Anurag <anurag8731@gmail.com>
2026-02-03 13:59:23 +0530

    Day 1 - JSX,Components,Props Basics

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function User(props) {
+  return (
+    <div>
+      <h3>User name : {props.name}</h3>
+      <p>User age : {props.age}</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <ul>User List</ul>
+      <li>
+        <User name="Anurag" age={38} />
+        <User name="Pragya" age={29} />
+      </li>
+    </div>
+  );
+}
+
+export default App;
