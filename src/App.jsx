fd4189fb0278d43b2f4e27789b028455f2bce376
Anurag <anurag8731@gmail.com>
2026-02-04 18:51:23 +0530

    "Day 10 - useCallback and React.memo optimization"

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
-import { useMemo, useState } from "react";
+import { useCallback, useState } from "react";
+import React from "react";
+
+const Child = React.memo(({ onClick }) => {
+  console.log("Child Rendered");
+  return <button onClick={onClick}>Click Me</button>;
+});
+
+const Child2 = React.memo(({ onClick }) => {
+  console.log("Child Rendered 2");
+  return <button onClick={onClick}>Click Me 2</button>;
+});
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [text,setText] = useState("");
+  const [count, setCount] = useState(0);
 
-  const calulation = useMemo(() => {
-    console.log("..Calculating");
-
-    let total = 0;
-    for(let i = 0; i < 1e8; i++) {
-      total += i;
-    }
-
-    return count * 2;
-  },[count])
+  const handleClick = useCallback(() => {
+    console.log("clicked");
+  }, []);
 
   return (
     <div>
-      <h1>{calulation}</h1>
+      <h1>{count}</h1>
 
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
 
-      <br /> <br />
+      <Child onClick={handleClick} />
 
-      <input 
-        type="text"
-        placeholder="Text here.."
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
+      <Child2 onClick={handleClick} />
     </div>
-  )
+  );
 }
 
 export default App;
